3bae8c94a5b6f9ebde078e9c1ec2fd87bd04c441
Vladimir Simonov <sim-steed2010@yandex.ru>
2023-12-28 10:22:33 +0300

    fix code

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
 // @todo: Темплейт карточки
 const places = document.querySelector('.places');
 const placeList = places.querySelector('.places__list');
-
+const cardTemplate = document.querySelector("#card-template").content;
 const deleteCard = (nodeElement) => nodeElement.remove();
 
-const createCard = ({ name, link, deleteCard }) =>{
-  const cardTemplate = document.querySelector("#card-template").content;
+const createCard = ({ name, link, deleteCard }) => {  
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardImageElement = cardElement.querySelector(".card__image");  
   cardElement.querySelector(".card__title").textContent = name;
   cardImageElement.src = link;
   cardImageElement.alt = `Фото ${name}`;
