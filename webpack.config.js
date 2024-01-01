65fcb8748b43cc4d03fbc5f5c85b8e1295548738
Vladimir Simonov <sim-steed2010@yandex.ru>
2024-01-01 12:22:35 +0300

    add config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
+  entry: { main: './src/index.js' },
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
         publicPath: ''
@@ -14,17 +16,19 @@ module.exports = {
     port: 8080,
     open: true
   },
-    module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+  module: {
+    rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/'
+        exclude: /node_modules/
       }
-      ]
-  }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+        new CleanWebpackPlugin(),
+  ]
 };
